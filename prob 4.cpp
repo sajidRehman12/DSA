74e146ecf41a5b8501982a996dcd573212dc3086
Sajid <rehmansajid919@gmail.com>
2025-07-26 22:32:24 +0500

    prob4

---
+++ prob 4.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<vector>
+#include<unordered_map>
+using namespace std;
+
+
+
+int minimumScore(vector<int>& nums, vector<vector<int>>& edges) 
+{
+        
+
+    
+
+}
--- problem3.cpp
+++ problem3.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<vector>
+#include<queue>
 using namespace std;
   int searchInsert(vector<int>& nums, int target) {
     int s=0, e=nums.size()-1,lastChanged=0;     
@@ -29,11 +30,41 @@ using namespace std;
 
         }
     }
+    if(nums[e]>target||nums[e]==target)
+     return e;
+    else{
+    
     return e+1;
+}
+}
+
+
+
+string makeFancyString(string s) {
+
+int j=1;
+string c= s[0];
+string ns;
+for(int i =1;i<s.size();i++)
+{
+    
+    if((s[i]==c)&&(j<3))
+    {
+        ns=ns+c;
+        j++;    
+    }
+    else
+    {
+        j=1;
+        c=s[i];
+    }
+
+}
+return ns;
 
 }
 int main()
 {vector <int >a={1,3,5,6};
 
-    cout<<searchInsert(a,2);
+    cout<<makeFancyString("aaabaaa");
 }
